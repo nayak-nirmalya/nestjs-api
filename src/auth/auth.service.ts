c323a73f37bd51407bd9490feaf4d587e6258598
Nirmalya Nayak <n.nayak263@gmail.com>
2023-06-21 21:23:08 +0530

    show custom error msg

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class AuthService {
@@ -11,17 +12,25 @@ export class AuthService {
     // generate hash
     const hash = await argon.hash(dto.password);
 
-    // save user in db
-    const user = await this.prisma.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
+    try {
+      // save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
 
-    // return saved user
-    return user;
+      // return saved user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError)
+        if (error.code === 'P2002')
+          throw new ForbiddenException('Credentials Taken!');
+
+      throw error;
+    }
   }
 
   signin() {
